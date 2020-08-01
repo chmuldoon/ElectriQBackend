07c724966f88573f4a46a2ec1cc085a91386672d
Charles Muldoon <charm3728@gmail.com>
2020-07-31 22:06:05 -0700

    removed unecessary code

--- App.js
+++ App.js
@@ -7,13 +7,6 @@ app.use(express.json({ extended: false }));
 app.use("/api/products", require("./routes/api/products"));
 
 
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 app.listen(process.env.PORT || 3000, () =>
   console.log(`Server started on ${process.env.PORT || 3000}`)
 );
